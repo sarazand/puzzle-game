117ebef1823b266f665656c419271698ee9cf5ab
sarazand <msarazandi@gmail.com>
2023-07-06 17:40:47 -0700

    create tiles and numbers

--- main.py
+++ main.py
@@ -11,8 +11,26 @@ class Game:
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         pygame.display.set_caption(title)
         self.clock = pygame.time.Clock()
+
+    def create_game(self):
+        grid = [[x + y * GAME_SIZE for x in range(1, GAME_SIZE + 1)]for y in range(GAME_SIZE)] 
+        grid[-1][-1] = 0
+        return grid
+
+    def draw_tiles(self):
+        self.tiles = []
+        for row, x in enumerate(self.tiles_grid):
+            self.tiles.append([])
+            for col, tile in enumerate(x):
+                if tile != 0:
+                    self.tiles[row].append(Tile(self, col, row, str(tile)))
+                else:
+                    self.tiles[row].append(Tile(self, col, row, "empty"))
+
     def new(self):
-        pass
+        self.all_sprites = pygame.sprite.Group()
+        self.tiles_grid = self.create_game()
+        self.tiles_grid_completed = self.create_game()
 
     def run(self):
         self.playing = True
@@ -22,21 +40,20 @@ class Game:
             self.draw()
             self.update()
 
-
-
     def update(self):
-        pass
+        self.all_sprites.update()
 
-    def draw_graid(self):
+    def draw_grid(self):
          for row in range(-1, GAME_SIZE * TILESIZE, TILESIZE):
               pygame.draw.line(self.screen, LIGHTGREY, (row,0), (row, GAME_SIZE*TILESIZE))
          for col in range(-1, GAME_SIZE * TILESIZE, TILESIZE):
               pygame.draw.line(self.screen, LIGHTGREY, (0,col), (GAME_SIZE*TILESIZE, col))
 
-
     def draw(self):
         self.screen.fill(BGCOLOR)
-        self.draw_graid()
+        self.all_sprites.draw(self.screen)
+        self.draw_grid()
+        self.draw_tiles()
         pygame.display.flip()
 
     def events(self):
